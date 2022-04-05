f09be0043a56e3b36ad2640f9acd0cd9b1410098
Matt Hallacy <mhallacy+github@haproxy.com>
2022-04-05 18:38:16 +0000

    Revert "More verbose errors, use rtm.connect instead of rtm.start"
    This reverts commit aacb4bfe4e208789c64ee031b9f754ef4cee139d.

--- lib/Slack/RTM/Bot/Client.pm
+++ lib/Slack/RTM/Bot/Client.pm
@@ -6,8 +6,6 @@ use warnings;
 use JSON;
 use Encode;
 use Data::Dumper;
-use Data::Printer;
-
 
 use HTTP::Request::Common qw(POST GET);
 use LWP::UserAgent;
@@ -26,7 +24,6 @@ my $ua = LWP::UserAgent->new(
 	}
 );
 $ua->agent('Slack::RTM::Bot');
-$ua->timeout(120);
 
 sub new {
 	my $pkg = shift;
@@ -41,15 +38,15 @@ sub connect {
 	my $self = shift;
 	my ($token) = @_;
 
-	my $res = $ua->request(POST 'https://slack.com/api/rtm.connect', [ token => $token ]);
+	my $res = $ua->request(POST 'https://slack.com/api/rtm.start', [ token => $token ]);
 	my $content;
 	eval {
 		$content = JSON::from_json($res->content);
 	};
 	if ($@) {
-		die 'connect response fail 00:'.Dumper $res->content;
+		die 'connect response fail:'.Dumper $res->content;
 	}
-	die 'connect response fail 1: '.$res->content unless ($content->{ok});
+	die 'connect response fail: '.$res->content unless ($content->{ok});
 
 	$self->{info} = Slack::RTM::Bot::Information->new(%{$content});
 	$res = $ua->request(POST 'https://slack.com/api/conversations.list ', [ token => $token ]);
@@ -57,9 +54,9 @@ sub connect {
 		$content = JSON::decode_json($res->content);
 	};
 	if ($@) {
-		die 'connect response fail 01:'.Dumper $res->content;
+		die 'connect response fail:'.Dumper $res->content;
 	}
-	die 'connect response fail 2: '.$res->content unless ($content->{ok});
+	die 'connect response fail: '.$res->content unless ($content->{ok});
 
 	for my $im (@{$content->{channels}}) {
 		$self->{info}->{channels}->{$im->{id}} = { %$im, name => '@'.$im->{name} };
@@ -96,9 +93,7 @@ sub _connect {
 			my ($cli, $error) = @_;
 			print STDERR 'error: '. $error;
 		});
-	print "preconnect\n";
-	p $ws_client->connect;
-	print "postconnect\n";
+	$ws_client->connect;
 
 	$self->{ws_client} = $ws_client;
 	$self->{socket} = $socket;
