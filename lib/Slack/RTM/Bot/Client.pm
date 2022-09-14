d14614eb292f907455036e11e7a2da872227c50d
kimjackson <kim.jackson@gmail.com>
2022-09-14 16:03:54 +1000

    Fix incorrect key in users.list response parsing
    The users.list API response contains the key `members`, not `users`.

--- lib/Slack/RTM/Bot/Client.pm
+++ lib/Slack/RTM/Bot/Client.pm
@@ -206,7 +206,7 @@ sub _refetch_users {
 		do {
 			$res = $ua->request(GET "https://slack.com/api/users.list?token=$self->{token}&cursor=$cursor");
 			my $args = JSON::from_json($res->content);
-			for my $user (@{$args->{users}}) {
+			for my $user (@{$args->{members}}) {
 				$users->{$user->{id}} = $user;
 			}
 			$cursor = $args->{response_metadata}->{next_cursor};
